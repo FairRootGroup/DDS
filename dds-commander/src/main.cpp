28d629304443379dbcea055ddd6b3506d35e1216
Anar Manafov <Anar.Manafov@gmail.com>
2014-06-11 15:45:46 +0200

    prototype of the dds-server command.

--- dds-commander/src/main.cpp
+++ dds-commander/src/main.cpp
@@ -50,20 +50,6 @@ bool ParseCmdLine(int _argc, char* _argv[], SOptions* _options) throw(exception)
     options.add_options()("stop", "Stop dds-commander daemon");
     options.add_options()("status", "Query current status of dds-command daemon");
 
-    //    options.add_options()("help,h", "Produce help message")("version,v", "Version information")("config,c", bpo::value<string>(), "A PoD configuration
-    // file")(
-    //        "mode,m", bpo::value<string>()->default_value("server"), "Agent's mode (use: server or worker)")("daemonize,d", "Run agent as a daemon")(
-    //        "start", "Start agent daemon (default action)")("stop", "Stop agent daemon")("status", "Query current status of agent daemon")(
-    //        "serverinfo", bpo::value<string>()->default_value("~/.PoD/etc/server_info.cfg"), "A server info file name")
-    //        // This parameter is mostly used by SSH plug-in to allow
-    //        // spawning several PROOF workers in one PoD session on a single worker node.
-    //        // The parameter can only be used by pod-agent workers and only when in
-    //        // the native PROOF connection.
-    //        ("workers",
-    //         bpo::value<unsigned int>(),
-    //         "A number of PROOF workers to spawn. Used only by agents in a worker mode and only in the native PROOF connection.");
-    //
-
     // Parsing command-line
     bpo::variables_map vm;
     bpo::store(bpo::parse_command_line(_argc, _argv, options), vm);
@@ -79,19 +65,6 @@ bool ParseCmdLine(int _argc, char* _argv[], SOptions* _options) throw(exception)
         PrintVersion();
         return false;
     }
-    //
-    //    if (!vm.count("config"))
-    //    {
-    //        cout << options << endl;
-    //        throw runtime_error("You need to specify a PoD configuration file at least.");
-    //    }
-    //
-    //    {
-    //        PoD::CPoDUserDefaults user_defaults;
-    //        user_defaults.init(vm["config"].as<string>());
-    //        _Options->m_podOptions = user_defaults.getOptions();
-    //    }
-    //
 
     MiscCommon::BOOSTHelper::conflicting_options(vm, "start", "stop");
     MiscCommon::BOOSTHelper::conflicting_options(vm, "start", "status");
