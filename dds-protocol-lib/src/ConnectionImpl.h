651c98a77f2b1fc7f78d575ba1625f8f93531c36
Anar Manafov <Anar.Manafov@gmail.com>
2014-07-24 19:10:24 +0200

    fixed code formatting

--- dds-protocol-lib/src/ConnectionImpl.h
+++ dds-protocol-lib/src/ConnectionImpl.h
@@ -101,7 +101,9 @@ namespace dds
         void close()
         {
             m_io_service.post([this]()
-                              { m_socket.close(); });
+                              {
+                                  m_socket.close();
+                              });
         }
 
         boost::asio::ip::tcp::socket& socket()
