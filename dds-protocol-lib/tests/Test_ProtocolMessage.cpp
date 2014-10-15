0dc37372be55ad9c87e0c06c1d808c0528213880
Anar Manafov <Anar.Manafov@gmail.com>
2014-10-15 20:30:51 +0200

    Fixed: Test_ProtocolMessage_cmdASSIGN_USER_TASK

--- dds-protocol-lib/tests/Test_ProtocolMessage.cpp
+++ dds-protocol-lib/tests/Test_ProtocolMessage.cpp
@@ -401,7 +401,7 @@ BOOST_AUTO_TEST_CASE(Test_ProtocolMessage_cmdASSIGN_USER_TASK)
     SAssignUserTaskCmd src;
     src.m_sExeFile = "test.exe -l -n --test";
     // expected attachment size
-    const unsigned int cmdSize = src.m_sExeFile.size() + 1;
+    const unsigned int cmdSize = src.m_sExeFile.size() + 1 + 2;
     MiscCommon::BYTEVector_t data;
     src.convertToData(&data);
     CProtocolMessage msg_src;
