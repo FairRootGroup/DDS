e06033f69447cf0c6d4aa29914fb158f4c54fd34
Anar Manafov <Anar.Manafov@gmail.com>
2014-10-15 20:25:52 +0200

    dds-protocol: Add availableOnWorker flag for tasks. (GH-6)

--- dds-protocol-lib/src/AssignUserTaskCmd.cpp
+++ dds-protocol-lib/src/AssignUserTaskCmd.cpp
@@ -12,16 +12,24 @@ namespace inet = MiscCommon::INet;
 
 void SAssignUserTaskCmd::normalizeToLocal() const
 {
+    m_nIsExeAvailableOnWorker = inet::_normalizeRead16(m_nIsExeAvailableOnWorker);
 }
 
 void SAssignUserTaskCmd::normalizeToRemote() const
 {
+    m_nIsExeAvailableOnWorker = inet::_normalizeWrite16(m_nIsExeAvailableOnWorker);
 }
 
 void SAssignUserTaskCmd::_convertFromData(const MiscCommon::BYTEVector_t& _data)
 {
+    size_t idx(0);
+    m_nIsExeAvailableOnWorker = _data[idx++];
+    m_nIsExeAvailableOnWorker += (_data[idx] << 8);
+
+    ++idx;
     vector<string> v;
     MiscCommon::BYTEVector_t::const_iterator iter = _data.begin();
+    advance(iter, idx);
     MiscCommon::BYTEVector_t::const_iterator iter_end = _data.end();
     for (; iter != iter_end;)
     {
@@ -45,6 +53,9 @@ void SAssignUserTaskCmd::_convertFromData(const MiscCommon::BYTEVector_t& _data)
 
 void SAssignUserTaskCmd::_convertToData(MiscCommon::BYTEVector_t* _data) const
 {
+    _data->push_back(m_nIsExeAvailableOnWorker & 0xFF);
+    _data->push_back(m_nIsExeAvailableOnWorker >> 8);
+
     copy(m_sExeFile.begin(), m_sExeFile.end(), back_inserter(*_data));
     _data->push_back('\0');
 }
--- dds-protocol-lib/src/AssignUserTaskCmd.h
+++ dds-protocol-lib/src/AssignUserTaskCmd.h
@@ -13,26 +13,29 @@ namespace dds
     struct SAssignUserTaskCmd : public SBasicCmd<SAssignUserTaskCmd>
     {
         SAssignUserTaskCmd()
+            : m_nIsExeAvailableOnWorker(0)
         {
         }
         void normalizeToLocal() const;
         void normalizeToRemote() const;
         size_t size() const
         {
-            return (m_sExeFile.size() + 1);
+            return (m_sExeFile.size() + 1 + sizeof(m_nIsExeAvailableOnWorker));
         }
         void _convertFromData(const MiscCommon::BYTEVector_t& _data);
         void _convertToData(MiscCommon::BYTEVector_t* _data) const;
         bool operator==(const SAssignUserTaskCmd& val) const
         {
-            return (m_sExeFile == val.m_sExeFile);
+            return (m_sExeFile == val.m_sExeFile && m_nIsExeAvailableOnWorker == val.m_nIsExeAvailableOnWorker);
         }
 
+        mutable uint16_t m_nIsExeAvailableOnWorker;
         std::string m_sExeFile;
     };
     inline std::ostream& operator<<(std::ostream& _stream, const SAssignUserTaskCmd& val)
     {
-        return _stream << "Exe: " << val.m_sExeFile;
+        return _stream << "Exe (" << (val.m_nIsExeAvailableOnWorker ? "local path" : "remote path")
+                       << "): " << val.m_sExeFile;
     }
     inline bool operator!=(const SAssignUserTaskCmd& lhs, const SAssignUserTaskCmd& rhs)
     {
