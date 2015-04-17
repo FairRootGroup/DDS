bf74594879f3f63e1e8c488646ab286d9a13123e
Andrey Lebedev <andrey.lebedev@gsi.de>
2015-04-17 14:08:30 +0200

    GH-74: dds-topology: report tasks and errors on different lines.

--- dds-topology/src/ActivateChannel.cpp
+++ dds-topology/src/ActivateChannel.cpp
@@ -103,8 +103,8 @@ bool CActivateChannel::on_cmdPROGRESS(SCommandAttachmentImpl<cmdPROGRESS>::ptr_t
 
         std::chrono::milliseconds timeToActivate(_attachment->m_time);
 
-        cout << "Tasks: " << _attachment->m_completed << " errors: " << _attachment->m_errors
-             << " total: " << _attachment->m_total
+        cout << "Activated tasks: " << _attachment->m_completed << "\nErrors: " << _attachment->m_errors
+             << "\nTotal: " << _attachment->m_total
              << "\nTime to Activate: " << std::chrono::duration<double>(timeToActivate).count() << " s" << endl;
     }
     return true;
