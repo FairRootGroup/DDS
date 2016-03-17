01ede415ec5370e26c5c746b2018acf60930bfe7
Andrey Lebedev <andrey.lebedev@gsi.de>
2016-03-17 14:28:41 +0100

    Remove obsolete localhost implementation

--- dds-submit/src/SubmitChannel.cpp
+++ dds-submit/src/SubmitChannel.cpp
@@ -22,59 +22,12 @@ CSubmitChannel::CSubmitChannel(boost::asio::io_service& _service)
     });
 
     subscribeOnEvent(EChannelEvents::OnHandshakeOK, [this](CSubmitChannel* _channel) {
-        if (m_sRMS != "localhost")
-        {
-            // Create the command's attachment
-            SSubmitCmd cmd;
-            cmd.m_sRMSType = m_sRMS;
-            cmd.m_sCfgFile = m_sCfgFile;
-            cmd.m_nNumberOfAgents = m_number;
-            pushMsg<cmdSUBMIT>(cmd);
-        }
-        else // localhost RMS requires special treatment
-        {
-            // Create temporary ssh configuration
-            const char* tmpdir = std::getenv("TMPDIR");
-            if (nullptr == tmpdir)
-            {
-                LOG(MiscCommon::log_stderr) << "$TMPDIR is not defined. To be able to use \'localhost\' please "
-                                               "define location of the temporary directory using TMPDIR "
-                                               "environment variable.";
-                return;
-            }
-            LOG(MiscCommon::log_stdout_clean) << "Using \'" << tmpdir << "\' to spawn agents.";
-            boost::filesystem::path tmpfileName(tmpdir);
-            tmpfileName /= "dds_ssh.cfg";
-            ofstream f(tmpfileName.string());
-
-            string userName;
-            MiscCommon::get_cuser_name(&userName);
-
-            boost::filesystem::path workingDirectoryPath(tmpdir);
-            workingDirectoryPath /= "dds-agents";
-            if (!boost::filesystem::exists(workingDirectoryPath) &&
-                !boost::filesystem::create_directory(workingDirectoryPath))
-            {
-                LOG(MiscCommon::log_stderr) << "Can't create working directory: " << workingDirectoryPath.string();
-                return;
-            }
-
-            stringstream ss;
-            ss << "wn, " << userName << "@localhost, ," << workingDirectoryPath.string() << ", "
-               << (m_number ? m_number : std::thread::hardware_concurrency());
-
-            LOG(MiscCommon::log_stdout_clean) << "SSH plug-in config:\n" << ss.str();
-
-            f << ss.str();
-
-            f.close();
-
-            // Create the command's attachment
-            SSubmitCmd cmd;
-            cmd.m_sRMSType = m_sRMS;
-            cmd.m_sCfgFile = tmpfileName.string();
-            pushMsg<cmdSUBMIT>(cmd);
-        }
+        // Create the command's attachment
+        SSubmitCmd cmd;
+        cmd.m_sRMSType = m_sRMS;
+        cmd.m_sCfgFile = m_sCfgFile;
+        cmd.m_nNumberOfAgents = m_number;
+        pushMsg<cmdSUBMIT>(cmd);
     });
 
     subscribeOnEvent(EChannelEvents::OnConnected, [](CSubmitChannel* _channel) {
