d8eefd1bd0b56cc4d5de41aec4d8e7fd8557797d
Andrey Lebedev <andrey.lebedev@gsi.de>
2019-12-02 13:57:56 +0100

    Add hash (CRC32) for the topology

--- MiscCommon/CRC.h
+++ MiscCommon/CRC.h
@@ -6,17 +6,63 @@
 #define CRC_H_
 
 // STD
+#include <fstream>
 #include <string>
 // BOOST
 #include <boost/crc.hpp>
 
 namespace MiscCommon
 {
-    inline uint64_t crc64(const std::string& _str)
+    using crc_optimal_64_t = boost::crc_optimal<64, 0x04C11DB7, 0, 0, false, false>;
+
+    template <class ret_t, class crc_t>
+    ret_t crc(const std::string& _str)
     {
-        boost::crc_optimal<64, 0x04C11DB7, 0, 0, false, false> crc;
+        crc_t crc;
         crc.process_bytes(_str.data(), _str.size());
         return crc.checksum();
+    }
+
+    inline uint32_t crc32(const std::string& _str)
+    {
+        return crc<uint32_t, boost::crc_32_type>(_str);
+    }
+
+    inline uint64_t crc64(const std::string& _str)
+    {
+        return crc<uint64_t, crc_optimal_64_t>(_str);
+    }
+
+    template <class ret_t, class crc_t>
+    ret_t crc(std::istream& _stream)
+    {
+        char buf[4096];
+        crc_t result;
+
+        do
+        {
+            _stream.read(buf, sizeof buf);
+            result.process_bytes(buf, _stream.gcount());
+        } while (_stream);
+
+        if (_stream.eof())
+        {
+            return result.checksum();
+        }
+        else
+        {
+            throw std::runtime_error("Failed to calculate CRC: file read failed");
+        }
+    }
+
+    inline uint32_t crc32(std::istream& _stream)
+    {
+        return crc<uint32_t, boost::crc_32_type>(_stream);
+    }
+
+    inline uint64_t crc64(std::istream& _stream)
+    {
+        return crc<uint64_t, crc_optimal_64_t>(_stream);
     }
 } // namespace MiscCommon
 
