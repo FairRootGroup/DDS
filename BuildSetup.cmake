53471def343e1314013d5e43cd770fc408d18a34
Anar Manafov <Anar.Manafov@gmail.com>
2022-05-29 08:26:40 +0200

    Change build type to Release with Debug info

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -19,7 +19,7 @@
 #
 # set cmake build type, default value is: RelWithDebInfo
 # possible options are: None Debug Release RelWithDebInfo MinSizeRel
-set( CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build" FORCE )
+set( CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "Choose the type of build" FORCE )
 
 #set(CMAKE_VERBOSE_MAKEFILE TRUE CACHE BOOL "This is useful for debugging only." FORCE)
 
